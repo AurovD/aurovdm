992ca5e8a9b9c0455fec63d391179e878283c2cf
AurovD <dmitry8891@mail.ru>
2021-02-23 19:58:57 +0300

    Added new projects

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const ctr = require("../controllers/index");
 router.get("/", ctr.main);
-router.get("/front", ctr.front);
-router.get("/back", ctr.back);
+router.get("/frontend", ctr.front);
+router.get("/backend", ctr.back);
 router.get("/design", ctr.des);
 router.get("/form", ctr.form);
 
