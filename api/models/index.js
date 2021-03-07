48543301f45a8163f6775584812d222eaee91129
AurovD <dmitry8891@mail.ru>
2021-03-07 21:08:08 +0300

    finished uploading

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,15 @@
 const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'babaduk',
+//     host: 'localhost',
+//     database: 'portfolio',
+//     password: '123',
+//     port: 5432,
+// });
 const pool = new Pool({
-    user: 'babaduk',
+    user: 'postgres',
     host: 'localhost',
-    database: 'portfolio',
+    database: 'postgres',
     password: '123',
     port: 5432,
 });
